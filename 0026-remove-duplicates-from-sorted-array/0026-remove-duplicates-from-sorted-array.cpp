8f72b8759cccdcd08fa4b45093f307913e283215
Suzanne Jolly <am1241079@iitd.ac.in>
2026-07-17 10:57:47 +0530

    Time: 52 ms (5.01%), Space: 22.7 MB (53.5%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for(int i=1;i<nums.size();){
+            if(nums[i]==nums[i-1]){
+                nums.erase(nums.begin()+i,nums.begin()+i+1);
+            }else{
+                i++;
+            }
+
+        }
+        return nums.size();
+        
+    }
+};
